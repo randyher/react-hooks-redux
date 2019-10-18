fafe8ab9f9a90991971268e80d704cfe108be530
github username <randyherasme6@gmail.com>
2019-10-18 13:30:08 -0400

    set todo state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [todos, setTodo] = useState([
+    {
+      text: "Updated Computer",
+      complete: false
+    },
+    {
+      text: "Is chilling",
+      complete: true
+    }
+  ]);
+
+  //Mapping
+
+  todos.map();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> Hey</p>
     </div>
   );
 }
