ddc00e9862b0db0fe18276344383a6c7d752f115
github username <randyherasme6@gmail.com>
2019-10-25 12:03:00 -0400

    another todo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import Todo from "./Todo.js";
 
 class App extends React.Component {
   state = {
-    todos: [{ id: 1, content: "Do Stuff" }, { id: 2, content: "Do More Stuff" }]
+    todos: [
+      { id: 1, content: "Do Stuff" },
+      { id: 2, content: "Do More Stuff" },
+      { id: 3, content: "This is another task" }
+    ],
+    redHey: false
   };
+
+  handleClick() {
+    this.setState({ redHey: !this.state.redHey });
+  }
+
   //Mapping
 
   render() {
@@ -12,6 +22,12 @@ class App extends React.Component {
       <div className="App">
         <h1 className="center blue-text">List</h1>
         <Todo todos={this.state.todos} />
+        <h1
+          style={this.state.redHey ? { color: "red" } : null}
+          onClick={this.handleClick}
+        >
+          HEY
+        </h1>
       </div>
     );
   }
